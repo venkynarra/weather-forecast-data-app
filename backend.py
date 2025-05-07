be55bfbeec9f48e4fe33c5a93c63d3aa9a6da08f
venkatesh <venkateshnarra368@gmail.com>
2025-05-07 17:38:57 -0500

    Day 33- filtering data , writing functions for temperature and sky for the next few days

--- backend.py
+++ backend.py
@@ -6,7 +6,14 @@ def get_data(place, forecast_days=None , kind=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     responses = requests.get(url)
     data = responses.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days # filtering the data by forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind == "temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+        if kind == "sky":
+            filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place= "Tokyo"))
+    print(get_data(place= "Tokyo", forecast_days=3, kind="Temperature"))
